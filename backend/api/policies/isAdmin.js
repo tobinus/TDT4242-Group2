7fcfd3751d89a3fa1f2d09cad57f62e71a0e9aeb
DeimosA <krvolden@gmail.com>
2018-02-22 04:25:20 +0100

    fix returned error messages in backend

--- backend/api/policies/isAdmin.js
+++ backend/api/policies/isAdmin.js
@@ -10,19 +10,19 @@ module.exports = function(req, res, next) {
 
   if (! req.session.authenticated) {
     // Not logged in
-    return forbidden();
+    return res.unauthorized({error: 'Not logged in'});
   }
 
   User.findOne(req.session.userId).exec(function (err, user) {
     if (err) return res.negotiate(err);
-    if (!user) return res.notFound();
+    if (!user) {
+      req.session.authenticated = false;
+      req.session.userId = null;
+      return res.unauthorized({error: 'Not logged in'});
+    }
 
     if (user.isAdmin) return next(); // User is admin so continue
-    return forbidden();
+    return res.forbidden({error: 'You are not permitted to perform this action'});
   });
-
-  function forbidden() {
-    return res.forbidden('You are not permitted to perform this action.');
-  }
 
 };
