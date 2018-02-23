9eca30b5121805a0614da0156fcbf88504089e87
DeimosA <krvolden@gmail.com>
2018-02-23 13:21:40 +0100

    mypage ease in

--- frontend/src/app/mypage/mypage.component.ts
+++ frontend/src/app/mypage/mypage.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserAuthService } from "../_shared/services/user-auth.service";
+import { UserModel } from "../_shared/app.models";
+
 @Component({
   selector: 'app-mypage',
   templateUrl: './mypage.component.html',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MypageComponent implements OnInit {
 
-  constructor() { }
+  private currentUser: UserModel;
+
+  constructor(
+    private userAuthService: UserAuthService,
+  ) { }
 
   ngOnInit() {
+    this.userAuthService.getCurrentUser().subscribe(
+      user => this.currentUser = user,
+      error => {/*Not logged in*/}
+    );
   }
 
 }
