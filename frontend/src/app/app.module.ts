647566be69e8e277279b04afdb0300bde482b2a0
Erling Hærnes Ihlen <erlingih@stud.ntnu.no>
2018-02-23 14:38:02 +0100

    finished rebase from login branch

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -21,22 +21,15 @@ import { ProductCreationComponent } from './product-creation/product-creation.co
     NavbarComponent,
     RegisterComponent,
     LoginComponent,
-<<<<<<< HEAD
     MypageComponent,
     ProductCreationComponent,
-=======
-    ProductCreationComponent
->>>>>>> 0d28fa820c684d59c965d97d0de183f57003cd17
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-<<<<<<< HEAD
     HttpClientModule,
     MaterializeModule,
-=======
->>>>>>> 0d28fa820c684d59c965d97d0de183f57003cd17
   ],
   providers: [UserAuthService],
   bootstrap: [AppComponent]
