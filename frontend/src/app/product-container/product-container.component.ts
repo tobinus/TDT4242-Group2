5394f6c1b2192cf5e60b7aa3d35cb6a68ec06666
Erling Hærnes Ihlen <erlingih@stud.ntnu.no>
2018-02-27 12:54:18 +0100

    early skeleton for product list

--- frontend/src/app/product-container/product-container.component.ts
+++ frontend/src/app/product-container/product-container.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductContainerComponent implements OnInit {
 
+  products = []
+  product_list_page = 0;
+  
   constructor() { }
 
   ngOnInit() {
+    // add products to the products[]
   }
-
+  
+  getProducts(): void{
+    // initial call to get products from service
+  }
+  
+  getNextProducts(): void{
+    // add +1 to product_list_page
+  }
 }
