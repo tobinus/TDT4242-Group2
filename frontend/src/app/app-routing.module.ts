f20f91705626ddf3b0110e27dbde72b51062f4f6
DeimosA <krvolden@gmail.com>
2018-03-03 17:28:03 +0100

    Redo product routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router"
+
 import { RegisterComponent } from "./register/register.component";
 import { LoginComponent } from "./login/login.component";
 import { MypageComponent } from "./mypage/mypage.component";
 import { NotFoundComponent} from "./errors/not-found/not-found.component";
-import {ProductCreationComponent} from "./product-creation/product-creation.component";
-
+import { ProductCreationComponent } from "./product-creation/product-creation.component";
 import { DealsComponent } from "./deals/deals.component";
 import { ProductContainerComponent } from "./product-container/product-container.component";
 import { ShoppingCartComponent } from "./shopping-cart/shopping-cart.component";
+import { ProductDetailsComponent } from "./product-details/product-details.component";
 
 const appRoutes: Routes = [
-  {
-    path: "newProduct",
-    component: ProductCreationComponent,
-  },
   {
     path: '',
     component: DealsComponent,
@@ -22,6 +19,18 @@ const appRoutes: Routes = [
   {
     path: 'products',
     component: ProductContainerComponent,
+  },
+  {
+    path: 'products/:productId',
+    component: ProductDetailsComponent,
+  },
+  {
+    path: "products/new",
+    component: ProductCreationComponent,
+  },
+  {
+    path: "products/:productId/edit",
+    component: ProductCreationComponent,
   },
   {
     path: 'cart',
